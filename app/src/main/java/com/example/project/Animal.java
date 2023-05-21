b8b1b83017d99834664ce792e1cfbcf0a0ea8c24
a22oscja <omo>
2023-05-21 20:49:57 +0200

    Implemented animal class

--- app/src/main/java/com/example/project/Animal.java
+++ app/src/main/java/com/example/project/Animal.java
@@ -1,4 +1,70 @@
 package com.example.project;
 
 public class Animal {
+
+    String ID, login, name, kingdom, aclass, order, family, description;
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getKingdom() {
+        return kingdom;
+    }
+
+    public void setKingdom(String kingdom) {
+        this.kingdom = kingdom;
+    }
+
+    public String getAclass() {
+        return aclass;
+    }
+
+    public void setAclass(String aclass) {
+        this.aclass = aclass;
+    }
+
+    public String getOrder() {
+        return order;
+    }
+
+    public void setOrder(String order) {
+        this.order = order;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public void setFamily(String family) {
+        this.family = family;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
