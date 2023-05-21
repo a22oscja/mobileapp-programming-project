73398da383244ce621b633e0e18faefd0948c0c6
a22oscja <omo>
2023-05-21 20:40:04 +0200

    Added code to support JsonTask and recyclerview

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -4,8 +4,22 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
+
+    private final String JSON_URL = "";
+    Gson gson = new Gson();
+    RecyclerView recyclerView;
+    ArrayList<Animal> animals = new ArrayList<Animal>();
+    CustomAdapter customAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +27,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        recyclerView = findViewById(R.id.recView);
+        customAdapter = new CustomAdapter(animals);
+        recyclerView.setAdapter(customAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        //new JsonTask(this).execute(JSON_URL);
     }
 
+    @Override
+    public void onPostExecute(String json) {
+        Type type = new TypeToken<ArrayList<Animal>>() {}.getType();
+        animals = gson.fromJson(json, type);
+        customAdapter.notifyDataSetChanged();
+    }
 }
