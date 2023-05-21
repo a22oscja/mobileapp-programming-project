a237a14333da1542713e95d0c821eb8a38477a83
a22oscja <omo>
2023-05-21 21:31:47 +0200

    layout adjustment + test code adjustment

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -37,11 +37,9 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
     }
 
     public void testingFunction() {
-        animals.add(new Animal("Elefant","Animalia","Mammalia","Proboscidea","Elephantidae","a\na\na\na\na\na\na\na\nb\nb\nb\nb\nb\n"));
-        animals.add(new Animal("Elefant","Animalia","Mammalia","Proboscidea","Elephantidae","a\na\na\na\na\na\na\na\nb\nb\nb\nb\nb\n"));
-        animals.add(new Animal("Elefant","Animalia","Mammalia","Proboscidea","Elephantidae","a\na\na\na\na\na\na\na\nb\nb\nb\nb\nb\n"));
-        animals.add(new Animal("Elefant","Animalia","Mammalia","Proboscidea","Elephantidae","a\na\na\na\na\na\na\na\nb\nb\nb\nb\nb\n"));
-        animals.add(new Animal("Elefant","Animalia","Mammalia","Proboscidea","Elephantidae","a\na\na\na\na\na\na\na\nb\nb\nb\nb\nb\n"));
+        for(int i = 0;i<10;i++) {
+            animals.add(new Animal("Elefant", "Animalia", "Mammalia", "Proboscidea", "Elephantidae", "Elephants are the largest existing land animals. Three living species are currently recognised: the African bush elephant, the African forest elephant, and the Asian elephant. They are the only surviving members of the family Elephantidae and the order Proboscidea. The order was formerly much more diverse during the Pleistocene, but most species became extinct during the Late Pleistocene epoch. Distinctive features of elephants include a long proboscis called a trunk, tusks, large ear flaps, pillar-like legs, and tough but sensitive skin. The trunk is used for breathing and is prehensile, bringing food and water to the mouth, and grasping objects. Tusks, which are derived from the incisor teeth, serve both as weapons and as tools for moving objects and digging. The large ear flaps assist in maintaining a constant body temperature as well as in communication. African elephants have larger ears and concave backs, whereas Asian elephants have smaller ears, and convex or level backs. "));
+        }
         customAdapter.updateAnimals(animals);
         customAdapter.notifyDataSetChanged();
     }
