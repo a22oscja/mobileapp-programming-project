210d207909cac4e278d8bd5aade781d4aa18f082
a22oscja <omo>
2023-05-23 22:12:18 +0200

    Button code and internet turned on

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -13,7 +16,7 @@ import com.google.gson.reflect.TypeToken;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
+public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener, View.OnClickListener {
 
     private final String JSON_URL = "";
     Gson gson = new Gson();
@@ -21,12 +24,17 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
     ArrayList<Animal> animals = new ArrayList<Animal>();
     CustomAdapter customAdapter;
 
+    Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(this);
         recyclerView = findViewById(R.id.recView);
         customAdapter = new CustomAdapter(animals);
         recyclerView.setAdapter(customAdapter);
@@ -50,5 +58,11 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         animals = gson.fromJson(json, type);
         customAdapter.updateAnimals(animals);
         customAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(MainActivity.this, About.class);
+        startActivity(intent);
     }
 }
